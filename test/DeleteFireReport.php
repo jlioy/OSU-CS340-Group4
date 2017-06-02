16ce2def44ca876e7793d50cc09d94f2e7485b76
Courtney Miller <millcour@oregonstate.edu>
2017-06-02 16:17:56 -0700

    adding new directory with working files

---
+++ test/DeleteFireReport.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="utf-8">
+   <link rel="stylesheet" href="css/style.css"/>
+   <title>Delete Fire Report</title>
+   <script type="text/javascript" src="js/hw1.js">
+   </script>
+</head>
+<body>
+	<?php include 'navigation.php' ?>
+	<div class="content">
+	<h1>Delete Fire Report</h1>
+	<div id="create">
+		<form method="post" action="DeleteFireProcess.php" onsubmit="return checkForm(this)">
+			<table id="signup" class="form">
+				<tr>
+					<td>Fire ID<br>
+						<input type="text" name="id">
+					</td>
+				</tr>
+				<tr>
+					<td>Location<br>
+						<input type="text" name="location">
+					</td>
+				</tr>
+			</table>
+			<button type="submit">Submit</button>
+		</form>
+		</div>
+		</div>
+	<?php include 'footer.php' ?>
+</body>
+</html>
---
+++ test/Home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="utf-8">
+   <link rel="stylesheet" href="css/style.css"/>
+   <title>Home</title>
+   <script type="text/javascript" src="js/hw1.js">
+   </script>
+</head>
+<body>
+	<?php include 'navigation.php' ?>
+	<div class="content">
+	<h1>Home</h1>
+	<p>
+		This website is designed for users to be able to create and see important dispatch information quickly, and conveniently.
+	</p>
+	</div>
+	<?php include 'footer.php' ?>
+</body>
+</html>
