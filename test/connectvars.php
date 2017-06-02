4f5c0e636b0ebba08c3d3b4791a6d3a3fe6f601f
Courtney Miller <millcour@oregonstate.edu>
2017-06-02 16:21:50 -0700

    removing unecessary directory

--- test/connectvars.php
+++
@@ -1,7 +0,0 @@
-<?php
-  // Define database connection constants
-  define('DB_HOST', 'classmysql.engr.oregonstate.edu');
-  define('DB_USER', 'cs340_haleyg');
-  define('DB_PASSWORD', '9184');
-  define('DB_NAME', 'cs340_haleyg');
-?>
--- test/footer.php
+++
@@ -1,6 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-	<footer>
-		Webpage Developed by Courtney Miller, Joshua Lioy, and Garrett Haley
-	</footer>
-</html>
